aee1838fa9c8b5209fbea9a5f00651172a426b46
Faris Dewantoro <farisd117@gmail.com>
2019-10-10 01:42:01 +0700

    hitungan beres

---
+++ WebApplication1/WebApplication1/Default.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="~/CSS/styles.css" rel="Stylesheet" type="text/css" />
+
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="wrapper">
+            <div class="wrapper_box">
+
+                <div class="wrapper_result" >
+                     <div class="wrapper_result--prev">
+                       <asp:Label ID="prevRes" runat="server" CssClass="text_prev" Text=""></asp:Label>
+                     </div>
+                    <div class="wrapper_result--curr">
+                       <asp:Label ID="result" runat="server" CssClass="text_result" Text="0"></asp:Label>
+                       <asp:Label ID="parentheses" runat="server" CssClass="text_parentheses" Text=""></asp:Label>
+                    </div>
+    
+                </div>
+             <div class="wrapper_button">
+                 <div class="wrapper_line">
+                     <asp:Button ID="btn7" runat="server" Text="7"  OnClick="btn_num"/>
+                        <asp:Button ID="btn8" runat="server" Text="8"  OnClick="btn_num"/>
+                     <asp:Button ID="btn9" runat="server" Text="9"  OnClick="btn_num"/>
+                      <asp:Button ID="btn_div" runat="server" Text=":"  OnClick="btn_oper"/>
+                 </div>
+                  <div class="wrapper_line">
+                     <asp:Button ID="btn4" runat="server" Text="4"  OnClick="btn_num"/>
+                        <asp:Button ID="btn5" runat="server" Text="5"  OnClick="btn_num"/>
+                     <asp:Button ID="btn6" runat="server" Text="6"  OnClick="btn_num"/>
+                      <asp:Button ID="btn_mul" runat="server" Text="x"  OnClick="btn_oper"/>
+                 </div>
+                          <div class="wrapper_line">
+                     <asp:Button ID="btn1" runat="server" Text="1"  OnClick="btn_num"/>
+                        <asp:Button ID="btn2" runat="server" Text="2"  OnClick="btn_num"/>
+                     <asp:Button ID="btn3" runat="server" Text="3"  OnClick="btn_num"/>
+                      <asp:Button ID="btn_sub" runat="server" Text="-"  OnClick="btn_oper"/>
+                 </div>
+                   <div class="wrapper_line">
+                     <asp:Button ID="btn0" runat="server" Text="0"  OnClick="btn_num"/>
+                        <asp:Button ID="parentheses1" runat="server" Text="("  OnClick="btnParenthesesOpen"/>
+                     <asp:Button ID="parentheses2" runat="server" Text=")"  OnClick="btnParenthesesClose"/>
+                      <asp:Button ID="btn_add" runat="server" Text="+"  OnClick="btn_oper"/>
+                 </div>
+                  <div class="wrapper_line--submit">
+                     <asp:Button ID="btnC" runat="server" Text="C"  OnClick="btn_c"/>
+                        <asp:Button ID="btnCE" runat="server" Text="CE"  OnClick="btn_ce"/>
+                     <asp:Button ID="btnEnter" runat="server" Text="Enter"  OnClick="btn_enter"/>
+                 </div>
+             </div>
+            
+
+           </div>
+        </div>
+     
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/Default.aspx.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+         public delegate bool Checker(string s);
+         public delegate string StrFunc(string s);
+         public static Regex RegexCheckOperator = new Regex(@"[xX*]|[:/]|\-|\+");
+        public static Regex RegexFullOper = new Regex(@"[xX*]|[:/]|\-|\+|\((.*)\)|\(|\)");
+         public static Regex RegexCheckParentheses = new Regex(@"\((.*)\)");
+         
+         public static Checker checkPrevIsOperator = (x => RegexCheckOperator.IsMatch(x[x.Length-1].ToString()));
+         public static StrFunc removeLast = (x => x.Substring(0, x.Length - 1));
+         public static StrFunc getLast = (x => x.Last().ToString());
+
+      
+        protected void Page_Load(object sender, EventArgs e)
+        {
+     
+        }
+
+        public static bool checkIsFirstIsZero(string s)
+        {
+            Regex regexZero = new Regex(@"^(0)");
+            string[] stringSplit = RegexFullOper.Split(s).Where(x=>x.Length > 0).ToArray();
+      
+            if(stringSplit.Length < 1)
+            {
+                return false;
+            }
+
+            return regexZero.IsMatch(stringSplit[stringSplit.Length-1]);
+        }
+        protected void btn_num(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if(result.Text == "0")
+            {
+                result.Text = btn.Text;
+            }
+            else if(getLast(result.Text) == ")")
+            {
+                result.Text += "*"+ btn.Text;
+            }
+            else if(getLast(result.Text) == "0")
+            {
+                result.Text = removeLast(result.Text) + btn.Text;
+            }
+            else
+            {
+                result.Text += btn.Text;
+            }
+            
+        }
+        
+        public static double evaluate(double a, double b, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '/':
+                    return a / b;
+                case '*':
+                    return a * b;
+                default:
+                    return 0;
+            }
+        }
+
+        public static double count(List<string> listStr)
+        {
+            try
+            {
+                List<double> stackNum = new List<double>();
+                foreach (var i in listStr)
+                {
+                    if (RegexCheckOperator.IsMatch(i))
+                    {
+                        double a = stackNum[stackNum.Count - 2];
+                        double b = stackNum[stackNum.Count - 1];
+                        double c = evaluate(a, b, Char.Parse(i));
+                        stackNum.RemoveRange(stackNum.Count - 2, 2);
+                        stackNum.Add(c);
+                    }
+                    else
+                    {
+                        stackNum.Add(Convert.ToDouble(i));
+                    }
+                }
+
+                if (stackNum.Any())
+                {
+                    return stackNum[0];
+                }
+                return 0;
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return 0;
+            }
+          
+            
+        }
+        public static List<string> convertPosition(string[] arr)
+        {
+            try
+            {
+
+            List<string> token = new List<string>(arr);
+            List<string> postStack = new List<string>();
+            List<string> operStack = new List<string>();
+            Dictionary<string, int> sortOper = new Dictionary<string, int>()
+            {
+                {"*",2 },
+                {"/",2 },
+                {"+",1 },
+                {"-",1 }
+            };
+
+            if (arr.Length == 0)
+            {
+                return postStack;
+            }
+
+            foreach (var elem in arr)
+            {
+                if (RegexCheckOperator.IsMatch(elem.ToString()))
+                {
+                   
+                    while (operStack.Any() && RegexCheckOperator.IsMatch(operStack.Last().ToString()) && sortOper[operStack.Last()] >= sortOper[elem])
+                    {
+                        var lastItem = operStack.Last();
+
+                        postStack.Add(lastItem);
+                        operStack.RemoveAt(operStack.Count -1);
+                    }
+                    operStack.Add(elem);
+                }
+                else if (elem == "(")
+                {
+                    operStack.Add(elem);
+                }
+                else if (elem == ")")
+                {
+                    while (operStack.Any() && operStack[operStack.Count -1] != "(")
+                    {
+                        var lastItem = operStack.Last();
+
+                        postStack.Add(lastItem);
+                        operStack.RemoveAt(operStack.Count - 1);
+                    }
+                    operStack.RemoveAt(operStack.Count - 1);
+                }
+                else
+                {
+                    postStack.Add(elem);
+                }
+
+            }
+            while (operStack.Any())
+            {
+                var lastItem = operStack.Last();
+                postStack.Add(lastItem);
+                operStack.RemoveAt(operStack.Count - 1);
+            }
+          
+            return postStack;
+
+            }
+            catch
+            {
+                return new List<string>() {"1","1","-" };
+            }
+        }
+
+        protected void btn_oper(object sender, EventArgs e)
+        {
+            Button btnOper = (Button)sender;
+          
+            if (checkPrevIsOperator(result.Text) || !RegexCheckOperator.IsMatch(btnOper.Text) || getLast(result.Text) == "(")
+            {
+                return;
+            }
+            else if(btnOper.ID == "btn_mul")
+            {
+                result.Text += "*";
+            }
+            else if (btnOper.ID == "btn_div")
+            {
+                result.Text += "/";
+            }
+            else
+            {
+                result.Text += btnOper.Text;
+            }
+        
+        }
+        protected void btnParenthesesOpen(object sender, EventArgs e)
+        {
+            Button btn_parantheses1 = (Button)sender;
+
+
+            if(getLast(result.Text) == "(")
+            {
+                return;
+            }
+            else if (result.Text == "0")
+            {
+                result.Text = $"({result.Text}";
+           
+            }
+            else if(!checkPrevIsOperator(result.Text) || getLast(result.Text) == ")")
+            {
+                result.Text += $"*{btn_parantheses1.Text}";
+             
+            }
+            else
+            {
+                result.Text += btn_parantheses1.Text;
+            }
+            parentheses.Text += ")";
+
+        }
+        protected void btnParenthesesClose(object sender, EventArgs e)
+        {
+            Button btn_parantheses2 = (Button)sender;
+            if (checkPrevIsOperator(result.Text) || getLast(result.Text) == "(")
+            {
+                return;
+            }
+            else if(parentheses.Text.Length > 0)
+            {
+                result.Text += btn_parantheses2.Text;
+                parentheses.Text = parentheses.Text.Substring(0, 0);
+            }
+         
+        }
+        protected void btn_enter(object sender, EventArgs e)
+        {
+            try
+            {
+                Regex RegexSplit = new Regex(@"([-+*/()])");
+                string[] strArray = RegexSplit.Split("23+3*5+(52+2*(4/2+0))").Where(x => x.Length > 0).ToArray();
+                if(strArray.Length < 1)
+                {
+                     result.Text = "0";
+                }
+                else
+                {
+                    List<string> stringList = convertPosition(strArray);
+                    result.Text = count(stringList).ToString();
+                }
+               
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                result.Text = "0";
+            }
+           
+          
+
+        }
+        protected void btn_c(object sender, EventArgs e)
+        {
+            result.Text = "0";
+            prevRes.Text = "";
+        }
+        protected void btn_ce(object sender, EventArgs e)
+        {
+            if(result.Text.Length > 1)
+            {
+                result.Text = removeLast(result.Text);
+            }
+         
+            
+        }
+    }
+}
